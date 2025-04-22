2fba23abdf0978863e72fb0efbbe440fa4a25085
S.Efimov <sys@tehnosvar.ru>
2025-04-22 09:46:18 +0300

    Add:

--- models.py
+++ models.py
@@ -1,11 +1,11 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, UUID
 from database import Base
-
+import uuid
 
 
 class Item(Base):
     __tablename__ = "items"
 
-    id = Column(Integer, primary_key=True, index=True)
+    uuid = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
     name = Column(String, index=True)
     description = Column(String, index=True)
--- tests/items.py
+++ tests/items.py
@@ -4,16 +4,11 @@ from datetime import datetime
 
 def post_request_ext():
     url = f"http://192.168.60.203:8001/items/"
-    for item in range(0, 2):
-
-        data = {"name": f"user-{ item }", "data": { "key": item }}
-
-        now = datetime.now()
-        formatted = now.strftime("%d.%m %H:%M:%Y")
+    for item in range(0, 10000):
 
         data = {
             "name": f"user-{ item }",
-            "description": f"{ formatted }"
+            "description": f"{ datetime.now() }"
         }
 
         response = requests.post(url, json=data)
